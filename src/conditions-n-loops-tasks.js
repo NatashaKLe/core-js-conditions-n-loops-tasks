17180b23142f6c56639250c3be71c64704baaa73
Natashakle <ataska91@gmail.com>
2024-02-07 20:40:45 +0300

    feat: add 8 tasks

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -227,8 +227,12 @@ function convertNumberToString(numberStr) {
  *  '0123210'   => true
  *  'qweqwe'    => false
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  let mirror = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    mirror += str[i];
+  }
+  return mirror === str;
 }
 
 /**
